95648f867254d75c5fe9fd5f3784b7b09c7d80bf
Yadin Dominguez <yadinds123@gmail.com>
2024-01-28 23:59:32 -0500

    Agregando test bascos

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,17 @@
 <?php
 
-it('returns a successful response', function () {
+use App\Http\Controllers\PostController;
+
+test('la ruta inicial retorna un estado de redireccion', function () {
     $response = $this->get('/');
 
-    $response->assertStatus(200);
+    $response->assertStatus(302);
 });
+
+test('la funcion index devuelve una lista de la api externa', function () {
+    $controller = new PostController();
+
+    $response = $controller->index();
+    expect($response->posts)->toBeArray();
+});
+
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,3 +3,7 @@
 test('that true is true', function () {
     expect(true)->toBeTrue();
 });
+
+// test('la ruta principal devuelve una coleccion' function () {
+    
+// })
